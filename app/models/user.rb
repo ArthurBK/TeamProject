76d2ed198f3eab427c532ad125e21f00112e2d1c
arthur bonnecarrere <arthurbonnecarrere@hotmail.fr>
2016-03-29 14:49:43 +0200

    with new ignore file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
   # mount_uploader :photo, PhotoUploader
   has_one :role
   validates :email, presence: true, format: {
